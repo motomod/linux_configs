1e7b1e22c9cbf3b73f9e2cbf9baddc8522d0ce76
Matt De'Ath <m@motomod.co.uk>
2023-04-17 18:44:50 +0100

    Install and bind jetbrains

---
+++ jetbrains/install-toolbox.sh
@@ -0,0 +1,4 @@
+cd ~/Downloads/
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.27.3.14493.tar.gz
+tar -xvf jetbrains-toolbox-1.27.3.14493.tar.gz
+sudo mv jetbrains-toolbox-1.27.3.14493/jetbrains-toolbox /usr/bin
