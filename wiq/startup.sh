730d4f2e4a1c157f0e49e612a809aa7d4f939df4
Matt De'Ath <m@motomod.co.uk>
2023-04-30 19:46:35 +0100

    Latest

--- wiq/startup.sh
+++ wiq/startup.sh
@@ -1,45 +1,46 @@
 #!/bin/sh
 
+sudo killall webpack
 sudo killall node
 sudo killall gulp
 
 cd /home/matt/Development/
 
 cd api
-docker-compose up -d
+docker compose up -d
 cd ..
 
 cd epos
-docker-compose up -d
+docker compose up -d
 cd ..
 
-cd admin
-docker-compose up -d
-cd ..
+#cd admin
+#docker compose up -d
+#cd ..
 
-cd payments
-docker-compose up -d
-cd ..
+#cd payments
+#docker compose up -d
+#cd ..
 
-cd proxy-service
-docker-compose up -d
-cd ..
+#cd proxy-service
+#docker compose up -d
+#cd ..
 
 cd back-office
-docker-compose up -d
+docker compose up -d
 yarn 
 yarn start &
 cd ..
 
 #cd batch-service
 #yarn
-#docker-compose up -d
+#docker compose up -d
 #cd ..
 
-cd connect
-docker-compose up -d
-cd ..
+#cd connect
+#docker compose up -d
+#cd ..
 
-cd wiq-client
-yarn start &
-cd ..
+#cd wiq-client
+#yarn start &
+#cd ..
